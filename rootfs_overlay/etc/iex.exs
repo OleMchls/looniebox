b6af2f89f3cfe9c48d609fb9776e2e004bd4dc90
Ole Michaelis <Ole.Michaelis@googlemail.com>
2018-08-31 08:36:12 +0200

    always attach logger

--- rootfs_overlay/etc/iex.exs
+++ rootfs_overlay/etc/iex.exs
@@ -1,5 +1,7 @@
 # Pull in Nerves-specific helpers to the IEx session
 use Nerves.Runtime.Helpers
 
+RingLogger.attach()
+
 # Be careful when adding to this file. Nearly any error can crash the VM and
 # cause a reboot.
