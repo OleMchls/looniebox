de36e0d08039cbf91f406f25ca592ee3a4716d0e
Ole Michaelis <Ole.Michaelis@googlemail.com>
2018-10-13 00:53:36 +0200

    flash lights on boot

--- lib/lohi/lights.ex
+++ lib/lohi/lights.ex
@@ -20,11 +20,34 @@ defmodule Lohi.Lights do
     {:ok, led0} = Led.start_link(20)
     {:ok, led1} = Led.start_link(26)
     {:ok, led2} = Led.start_link(16)
-    {:ok, led3} = Led.start_link(13)
-    {:ok, led4} = Led.start_link(6)
+    {:ok, led3} = Led.start_link(6)
+    {:ok, led4} = Led.start_link(13)
 
     {:ok, [led0, led1, led2, led3, led4]}
   end
 
-  def handle_call({:flash}, _from, leds), do: {:reply, Enum.map(leds, &Led.on/1), leds}
+  def handle_call({:flash}, _from, [led0, led1, led2, led3, led4] = leds) do
+    Process.send_after(self(), {:on, led0}, 0)
+    Process.send_after(self(), {:on, led1}, 500)
+    Process.send_after(self(), {:on, led2}, 1000)
+    Process.send_after(self(), {:on, led3}, 1500)
+    Process.send_after(self(), {:on, led4}, 2000)
+
+    Enum.each(leds, fn led ->
+      Process.send_after(self(), {:off, led}, 2500)
+      Process.send_after(self(), {:on, led}, 2800)
+    end)
+
+    {:reply, true, leds}
+  end
+
+  def handle_info({:on, led}, state) do
+    Led.on(led)
+    {:noreply, state}
+  end
+
+  def handle_info({:off, led}, state) do
+    Led.off(led)
+    {:noreply, state}
+  end
 end
