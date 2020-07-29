e8c19906b84ba995390d31f493ee542123e8c2c3
Ole Michaelis <Ole.Michaelis@googlemail.com>
2020-07-29 10:01:04 +0200

    use lan local hostnames

--- lib/looniebox.ex
+++ lib/looniebox.ex
@@ -1,7 +1,7 @@
 defmodule Looniebox do
   def start_node do
     {:ok, hostname} = :inet.gethostname()
-    Node.start(:"nerves@#{hostname}.local")
+    Node.start(:"nerves@#{hostname}.")
   end
 
   def scanned(uuid), do: LohiUi.Controls.tag(uuid)
